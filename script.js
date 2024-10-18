69ea4c8708028eef66cb1920c7a16f41b1d96ba9
chris <21200055@ue.edu.pe>
2024-10-18 16:57:11 -0500

    holakace

--- script.js
+++ script.js
@@ -5,8 +5,39 @@ numberInput.addEventListener('input', function () {
     this.value = this.value.replace(/\D/g, '');
 });
 
+function sendData() {
+  const data = {
+    name: document.getElementById('name').value,
+    lnam: document.getElementById("apellido").value, 
+    email: document.getElementById('email').value,
+    code: document.getElementById("codigo").value,
+    teach: document.getElementById("profesor").value,
+    sche: document.querySelector('input[name="rating"]:checked')?.value
+  };
+
+  fetch('WEB_APP_URL', {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  .then(response => response.json())
+  .then(response => {
+    if (response.status === 'success') {
+      alert('Formulario enviado exitosamente.');
+    } else {
+      alert('Ocurrió un error al enviar el formulario. Inténtalo nuevamente.');
+    }
+  })
+  .catch(error => {
+    console.error('Error:', error);
+    alert('Error al enviar el formulario.');
+  });
+}
+
 document.getElementById('surveyForm').addEventListener('submit', function(event) {
   event.preventDefault();
   alert('¡Formulario enviado exitosamente!');
-  // Aquí puedes añadir la lógica para enviar los datos a un servidor
+  sendData();
 });
